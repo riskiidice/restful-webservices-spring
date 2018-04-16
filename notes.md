281c28c9fed77969ddd93e1bcedc222ec3b6cc30
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-04-16 10:37:13 +0700

    Finished XML negotiation

--- notes.md
+++ notes.md
@@ -12,3 +12,16 @@
 - Retrieve all posts for a User   - GET /users/{id}/posts
 - Create a post for User		     - POST /users/{id}/posts
 - Retrieve details of a post		 - GET  /users/{id}/posts/{post_id}
+
+
+#### Internationalization
+
+##### Configuration
+- LocaleResolver
+	- Default Locale - Locale.US
+- ResourceBundleMessageSource
+
+##### Usage
+- Autowire MessageSource
+- @RequestHeader(value = "Accept-Language", required = false) Locale locale
+- messageSource.getMessage("HelloWorld.message", null, locale)
--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.example.demo.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	public MessageSource messageSource;
+	
 	@GetMapping(path="/hello-world")
 	public String HelloWorld() {
 		return "Hello World";
@@ -21,5 +30,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationlized")
+	public String HelloWorldInternationlized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
